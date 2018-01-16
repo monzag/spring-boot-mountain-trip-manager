6d3fe63fd478c8cd4b83344d2cd1517f5a1475a1
monzag <zaganiacz.m@gmail.com>
2018-01-16 17:17:42 +0100

    Add MountainTripManagerApplication

---
+++ src/main/java/com/monzag/mountaintripmanager/MountainTripManagerApplication.java
@@ -0,0 +1,12 @@
+package com.monzag.mountaintripmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainTripManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountainTripManagerApplication.class, args);
+	}
+}
