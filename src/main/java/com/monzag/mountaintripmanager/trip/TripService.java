3b2e6b44cbe3bc50a358b1bd1c969a7d583ca690
monzag <zaganiacz.m@gmail.com>
2018-01-16 15:33:29 +0100

    Add TripService

---
+++ src/main/java/com/monzag/mountaintripmanager/trip/TripService.java
@@ -0,0 +1,21 @@
+package com.monzag.mountaintripmanager.trip;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TripService {
+
+    private TripRepository tripRepository;
+
+    public TripService(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    public Trip get(Integer id) {
+        Trip trip = tripRepository.findOne(id);
+        if (trip == null) {
+            System.out.println("Exception: trip not exist");
+        }
+        return trip;
+    }
+}
