b890ac62d253e5250122a8e467f140d81613b850
monzag <zaganiacz.m@gmail.com>
2018-01-16 14:53:01 +0100

    Add get method

--- src/main/java/com/monzag/mountaintripmanager/trip/TripController.java
+++ src/main/java/com/monzag/mountaintripmanager/trip/TripController.java
@@ -1,6 +1,7 @@
 package com.monzag.mountaintripmanager.trip;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class TripController {
     @GetMapping(path = "")
     public Iterable<Trip> index() {
         return new ArrayList<>();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Trip get(@PathVariable Integer id) {
+        return new Trip();
     }
 }
