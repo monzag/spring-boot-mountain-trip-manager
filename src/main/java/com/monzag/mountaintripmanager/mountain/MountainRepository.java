db4fbf7e532691517b517712491c4677efd2dd9e
monzag <zaganiacz.m@gmail.com>
2018-01-16 16:04:42 +0100

    Add MountainRepository

---
+++ src/main/java/com/monzag/mountaintripmanager/mountain/MountainRepository.java
@@ -0,0 +1,8 @@
+package com.monzag.mountaintripmanager.mountain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MountainRepository extends CrudRepository<Mountain, Integer> {
+}
