990f229f2f0ab304a46a734ea5e60193a03a3b40
monzag <zaganiacz.m@gmail.com>
2018-01-17 17:04:20 +0100

    Add findByArchived and findByIdAndArchived

--- src/main/java/com/monzag/mountaintripmanager/mountain/MountainRepository.java
+++ src/main/java/com/monzag/mountaintripmanager/mountain/MountainRepository.java
@@ -5,4 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MountainRepository extends CrudRepository<Mountain, Integer> {
+
+    Iterable<Mountain> findByArchived(Boolean archieved);
+
+    Mountain findByIdAndArchived(Integer id, Boolean archieved);
 }
