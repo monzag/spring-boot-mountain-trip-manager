70076bb18411705dc3abff45485e12710e0af2d6
monzag <zaganiacz.m@gmail.com>
2018-01-17 14:42:41 +0100

    Add findByArchived and findByIdAndArchived

--- src/main/java/com/monzag/mountaintripmanager/trip/TripRepository.java
+++ src/main/java/com/monzag/mountaintripmanager/trip/TripRepository.java
@@ -3,6 +3,12 @@ package com.monzag.mountaintripmanager.trip;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface TripRepository extends CrudRepository<Trip, Integer> {
+
+    Iterable<Trip> findByArchived(Boolean archived);
+
+    Trip findByIdAndArchived(Integer id, Boolean archived);
+
 }
