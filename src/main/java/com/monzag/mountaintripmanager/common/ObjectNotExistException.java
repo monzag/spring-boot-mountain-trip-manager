bc52e9ebc0523a30f66e781a01e151919c8db649
monzag <zaganiacz.m@gmail.com>
2018-01-18 21:20:28 +0100

    Add constructor to ObjectNotExistException

--- src/main/java/com/monzag/mountaintripmanager/common/ObjectNotExistException.java
+++ src/main/java/com/monzag/mountaintripmanager/common/ObjectNotExistException.java
@@ -2,6 +2,10 @@ package com.monzag.mountaintripmanager.common;
 
 public class ObjectNotExistException extends Exception {
 
+    public ObjectNotExistException(String message) {
+        super(message);
+    }
+
     public ObjectNotExistException() {
         super("Object with the given id not exist");
     }
