63b5285a8c76f403bb7ca07a17a7d4ce52c581f1
monzag <zaganiacz.m@gmail.com>
2018-01-18 22:50:37 +0100

    Add getResponse method and refactor handleException

--- src/main/java/com/monzag/mountaintripmanager/common/HandlerException.java
+++ src/main/java/com/monzag/mountaintripmanager/common/HandlerException.java
@@ -1,6 +1,9 @@
 package com.monzag.mountaintripmanager.common;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,10 +18,27 @@ public class HandlerException extends ResponseEntityExceptionHandler {
         this.logger = logger;
     }
 
+    @ExceptionHandler(ArchivedObjectException.class)
+    public ResponseEntity<String> handleException(ArchivedObjectException e) throws JSONException {
+        logger.error("User error: object with provide id is archived");
+        return getResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ObjectNotExistException.class)
-    public String handleException(ObjectNotExistException e) {
-        logger.error("User error: object with provide id not exist or is archived");
-        return e.getMessage();
+    public ResponseEntity<String> handleException(ObjectNotExistException e) {
+        logger.error("User error: object with provide id not exist");
+        return getResponse(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<String> getResponse(String message, HttpStatus status) {
+        JSONObject response = new JSONObject();
+        try {
+            response.put("status", status.toString());
+            response.put("message", message);
+        } catch (JSONException e) {
+            logger.error("Program error: json response can't be create");
+        }
+        return new ResponseEntity<>(response.toString(), status);
     }
 }
